66a3fdca281bce340802a0e4008ea96747ddc928
Cristina Diaz <hagaren_022@hotmail.com>
2022-04-13 17:43:40 -0400

    dos

--- Prueba/assets/js/index.js
+++ Prueba/assets/js/index.js
@@ -1,35 +1,13 @@
-
-
-
-
 $(document).ready(function () {
 
-    $("a").click(function () {
-        var gato = this.hash
 
-
-        $("html, body").animate(
-            {
-                schrolltop: $(gato).offset().top - 70
-            },
-            800
-
-        )
-    })
-
-
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    /*tooltip para los íconos de la sección "Quienes somos" */
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
 
 
-    var myModal = document.getElementById('myModal')
-    var myInput = document.getElementById('myInput')
-
-    myModal.addEventListener('shown.bs.modal', function () {
-        myInput.focus()
-    })
 
 
 })
+
