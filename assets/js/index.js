3e4c8d9e2959eca7d5ad4c22db60f88bedfbc6b8
Cristina Diaz <hagaren_022@hotmail.com>
2022-04-14 09:15:33 -0400

    arreglo de footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
 
 
-    /*tooltip para los íconos de la sección "Quienes somos" */
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-        return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
+  /*tooltip para los íconos de la sección "Quienes somos" */
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
 
-    $(document).ready(function(){
-        $(".card-title").click(function(){
-          $(this).hide();
-        });
-      });
+  $(document).ready(function () {
+    $(".card-title").click(function () {
+      $(this).hide();
+    });
+  });
 
-
+  
 })
 
